dbf64820b8c3d644f6135a86db6ccba420f45e05
丁元皓 <protomyst@outlook.com>
2025-01-12 18:16:45 +0800

    添加网站图标

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <title>IP 检测工具</title>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
